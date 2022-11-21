8f927a1d088235aca448fa15df6abff43ac7f299
anushkumarv <anush070@gmail.com>
2022-11-21 16:49:13 -0700

    Updated clip dataset

--- dataset/clip_dataset.py
+++ dataset/clip_dataset.py
@@ -19,11 +19,12 @@ class TrainDatasetOnline(data.Dataset):
         self.transforms = transforms
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.model, self.preprocess = clip.load(config.get('clip_backend'), device=self.device)
-        self.train_image_emb = None 
-        self.train_text_emb = None
+        self.train_compress_net_emb = None 
+        self.train_query_net_emb = None
+        self._prepare_embeddings()
 
     def __getitem__(self, index):
-        return self.train_image_emb[index], self.train_text_emb[index]
+        return self.train_compress_net_emb[index], self.train_query_net_emb[index]
 
     def __len__(self):
         return self.train_image_emb.shape[0]
@@ -32,17 +33,20 @@ class TrainDatasetOnline(data.Dataset):
         try:
             image = Image.open(requests.get(self.config.get('url_root') + img_id + '.jpg', stream=True).raw)
         except Exception as e:
-            return None
+            raise Exception
         if self.transform is not None:
             image = self.transform(image)
 
         return image
 
-    def _get_image_embedding(self, image):
-        image = self.preprocess(image).unsqueeze(0).to(self.device)
+    def _get_image_embedding(self, src_image, tgt_image, ntgt_image):
+        src_image = self.preprocess(src_image).unsqueeze(0).to(self.device)
+        tgt_image = self.preprocess(tgt_image).unsqueeze(0).to(self.device)
+        ntgt_image = self.preprocess(ntgt_image).unsqueeze(0).to(self.device)
+        all_image = torch.vstack((src_image,tgt_image,ntgt_image))
         with torch.no_grad():
-            image_features = self.model.encode_image(image)
-        return image_features
+            all_image_features = self.model.encode_image(all_image)
+        return all_image_features
 
     def _preprocess_text(self, text):
         return re.sub(r'[^A-Za-z0-9 ]+', '', text).lower()
@@ -58,9 +62,34 @@ class TrainDatasetOnline(data.Dataset):
 
         return text_emb
 
+    def _stack_embeddings(self, all_img_emb, text_em):
+        src_img_em, tgt_ntgt_img_emb = torch.split(all_img_emb, [1,2])
+        tgt_ntgt_img_emb = tgt_ntgt_img_emb.unsqueeze(0)
+        query_em = torch.cat((src_img_em, text_em), dim=1).unsqueeze(0)
+
+        if self.train_compress_net_emb:
+            self.train_compress_net_emb = torch.vstack((self.train_compress_net_emb,tgt_ntgt_img_emb))
+        else:
+            self.train_compress_net_emb = tgt_ntgt_img_emb
+
+        if self.train_query_net_emb:
+            self.train_query_net_emb = torch.vstack((self.train_query_net_emb, query_em))
+        else:
+            self.train_query_net_emb = query_em
+
     def _prepare_embeddings(self):
         src_file = os.path.join(self.config.get('data_root') + self.config.get('train_csv'))
         df = pd.read_csv(src_file)
+        for _, row in df.iterrows:
+            try:
+                all_img_emb = self._get_image_embedding(self._download_image(row['Source Image ID']), 
+                                        self._download_image(row['Target Image ID']),
+                                        self._download_image(row['Non-Target Image ID']))
+                txt_em = self._get_text_embeddings(row['Feedback 1'], row['Feedback 2'], row['Feedback 3'])
+                self._stack_embeddings(all_img_emb, txt_em)
+            except Exception as e:
+                continue
+
 
 
 
