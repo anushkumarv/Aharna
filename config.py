d7bcb642850a544a5f9d2df681b57ae609c2d745
anushkumarv <anush070@gmail.com>
2022-11-20 21:22:18 -0700

    boilerplate code

---
+++ config.py
@@ -0,0 +1,11 @@
+config = {
+    # training files
+    'data_root': './data',
+    'train_csv': 'apparel_train_annotation.csv',
+    'dev_csv': '',
+    'test_csv': '',
+    'dev_json': 'query_file_released.jsonl',
+    'train_img': '',
+    'dev_img': '',
+    'test_img': ''
+}
